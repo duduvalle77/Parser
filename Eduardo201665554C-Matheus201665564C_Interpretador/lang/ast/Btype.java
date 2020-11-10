05ef0dc713a137fda554ee167b1628336b13c390
duduvalle77 <duduvalle77@yahoo.com.br>
2020-11-10 01:03:16 -0300

    Sumiu

--- Eduardo201665554C-Matheus201665564C_Interpretador/lang/ast/Btype.java
+++
@@ -1,8 +0,0 @@
-/* Eduardo Vieira Marques Pereira do Valle 201665554C
- * Matheus Brinati Altomar 201665564C
- */
-package lang.ast;
-
-public abstract class Btype extends SuperNode{
-    
-}
--- Eduardo201665554C-Matheus201665564C_Interpretador/lang/ast/Seletor.java
+++
@@ -1,9 +0,0 @@
-/* Eduardo Vieira Marques Pereira do Valle 201665554C
- * Matheus Brinati Altomar 201665564C
- */
-package lang.ast;
-
-
-public abstract class Seletor extends SuperNode{
-    
-}
