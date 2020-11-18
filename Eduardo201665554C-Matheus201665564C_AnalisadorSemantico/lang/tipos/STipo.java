958cedf3883c525f3f77e17f4c98517660ad74ea
duduvalle77 <duduvalle77@yahoo.com.br>
2020-11-18 00:41:36 -0300

    Sumiu

--- Eduardo201665554C-Matheus201665564C_AnalisadorSemantico/lang/tipos/STipo.java
+++
@@ -1,9 +0,0 @@
-/* Eduardo Vieira Marques Pereira do Valle 201665554C
- * Matheus Brinati Altomar 201665564C
- */
-package lang.tipos;
-
-public abstract class STipo
-{
-    public abstract boolean match( STipo v );
-}
